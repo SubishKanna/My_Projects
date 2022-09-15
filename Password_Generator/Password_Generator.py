35c7a3f1a3b31a0bafd40047c39d5914aed98871
subish <subishkanna@gmail.com>
2022-09-15 16:38:25 +0530

    New initial commit

--- Password_Generator/Password_Generator.py
+++ Password_Generator/Password_Generator.py
@@ -1,31 +1,34 @@
-#Password Generator Project
+"""
+PASSWORD GENERATOR
+
+This program allows you to generate password based on your input (no of letters, symbols, numbers)
+
+"""
+
 import random
 from Password_Generator_DB import letters, numbers, symbols
 
+print("Hello.....Welcome to the Password Generator!!")
+n_letters = int(input("How many letters would you like in your password?\n"))
+n_symbols = int(input("How many symbols would you like in  your password?\n"))
+n_numbers = int(input("How many numbers would you like in  your password?\n"))
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input("How many symbols would you like?\n"))
-nr_numbers = int(input("How many numbers would you like?\n"))
-
-
-total_len = nr_letters+nr_symbols+nr_numbers
-r_letter = ''
-r_number = ''
-r_symbol = ''
-password = ''
+total_len = n_letters + n_symbols + n_numbers
+random_letters = ''
+random_numbers = ''
+random_symbols = ''
+your_password = ''
 
 for i in range(total_len):
-    if i<nr_letters:
-        r_letter += letters[random.randint(0,len(letters)-1)]
-    if i<nr_numbers:
-        r_number += numbers[random.randint(0,len(numbers)-1)]
-    if i<nr_symbols:
-        r_symbol += symbols[random.randint(0,len(symbols)-1)]
+    if i < n_letters:
+        random_letters += letters[random.randint(0, len(letters) - 1)]
+    if i < n_numbers:
+        random_numbers += numbers[random.randint(0, len(numbers) - 1)]
+    if i < n_symbols:
+        random_symbols += symbols[random.randint(0, len(symbols) - 1)]
 
-password = r_letter+r_number+r_symbol
-print("Here's your generated passsword\n",password)
+your_password = random_letters + random_numbers + random_symbols
+print("Here's your generated password\n", your_password)
 
-pass_shuf =''.join(random.sample(password,len(password)))
-print("Here's your generated shuffled passsword\n",pass_shuf)
-
+shuffled_password = ''.join(random.sample(your_password, len(your_password)))
+print("Here's your generated shuffled password\n", shuffled_password)
